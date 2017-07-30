1653d39fe79ce98dd9368dda73dc2c00ce7f85a8
torgard <aksel@akseltorgard.com>
2017-07-30 19:42:16 +0100

    Renamed all function to find

--- src/db/device.js
+++ src/db/device.js
@@ -2,9 +2,9 @@ const v4 = require('uuid').v4;
 const Device = require('./models').device;
 const execute = require('./models').execute;
 
-const all = userId => Device.find({ userId });
+const find = userId => Device.find({ userId });
 
 const newDevice = userId => new Device({ userId, deviceId: v4() }).save();
 
 module.exports.new = (userId, cb, err) => execute(newDevice(userId), cb, err);
-module.exports.all = (userId, cb, err) => execute(all(userId), cb, err);
+module.exports.find = (userId, cb, err) => execute(find(userId), cb, err);
