7d3444e0a700ea32dc08e844f36affcdd32cbc68
torgard <aksel@akseltorgard.com>
2017-09-03 21:16:38 +0200

    Added scheduleNotification function.

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -1,6 +1,14 @@
 const Agenda = require('agenda');
 const connectionString = require('../db/config');
 
+const sendNotification = require('../socketServer').sendNotification;
+
 const agenda = new Agenda({ db: { address: connectionString, collection: 'agendaJobs' }});
 
-module.exports = agenda;
+agenda.define('notification', { priority: 'highest' }, (job) => sendNotification(job.attrs.data.notification));
+
+const scheduleNotification = (notification, when) => {
+  agenda.schedule(when, 'notification', { notification });
+};
+
+module.exports.scheduleNotification = scheduleNotification;
